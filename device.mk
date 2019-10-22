ea1520cf1ead19f7ec6212f7d4a8887fa6ae7823
perkabalo <perkabalo@gmail.com>
2019-10-22 18:47:31 +0200

    Revert "Migrate to livedisplay 2.0"
    This reverts commit 784329666ed2c6b60b2600c4bfd2274587fad22b.

--- device.mk
+++ device.mk
@@ -276,10 +276,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm660
